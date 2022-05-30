4f669455108dc6b37a6dd26d96dd2100da31e678
lh <2731164534@qq.com>
2022-05-30 14:34:27 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			if (!wx.cloud) {
+				console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+			} else {
+				wx.cloud.init({
+					env: 'lh-xcxkfds-3g3e3agda56584c9',
+					traceUser: true,
+				});
+			}
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,72 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<button type="default" @click="test()">点击</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			test(){
+				wx.showLoading({
+				  title: '加载中',
+				  mask:true
+				})
+				wx.cloud.callFunction({
+				  // 云函数名称
+				  name: 'test',
+				  // 传给云函数的参数
+				  data: {
+				    a: 1,
+				    b: 2,
+				  },
+				})
+				.then(res => {
+				  console.log(res.result) // 3
+				  wx.hideLoading()
+				})
+				.catch(console.error)
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
